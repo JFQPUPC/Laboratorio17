ffea96722e4f4ac79812359ea13e465597a20fe6
JFQPUPC <jfelipequintero@unicesar.edu.co>
2024-12-08 13:41:53 -0500

    HAVE

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-
-document.querySelector('button').addEventListener('click', function() {
+document.querySelector('button').addEventListener('click', function () {
     alert('¡Bienvenido a mi sitio web personal!');
 });
 
 const form = document.getElementById('contactForm');
-form.addEventListener('submit', function(event) {
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    
+form.addEventListener('submit', function (event) {
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+
     if (name === '' || email === '') {
         alert('Por favor, completa todos los campos.');
-        event.preventDefault(); 
+        event.preventDefault();
     } else {
         alert('¡Formulario enviado correctamente!');
     }
